d25c014b6a72dd3382ca6b6760d3ab328cd22cb9
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-08-30 06:46:41 -0400

    Make sure inits are public

--- Sources/Extendable/AppExtensionBrowserView.swift
+++ Sources/Extendable/AppExtensionBrowserView.swift
@@ -3,6 +3,9 @@ import SwiftUI
 
 @available(macOS 13.0, *)
 public struct AppExtensionBrowserView: NSViewControllerRepresentable {
+	public init() {
+	}
+	
 	public func makeNSViewController(context: Context) -> EXAppExtensionBrowserViewController {
 		return EXAppExtensionBrowserViewController()
 	}
--- Sources/Extendable/ExtensionHostingView.swift
+++ Sources/Extendable/ExtensionHostingView.swift
@@ -3,15 +3,13 @@ import SwiftUI
 
 @available(macOS 13.0, *)
 public struct ExtensionHostingView: NSViewControllerRepresentable {
-	public typealias NSViewControllerType = EXHostViewController
+	public let configuration: EXHostViewController.Configuration
 
-	let configuration: EXHostViewController.Configuration
-
-	init(configuration: EXHostViewController.Configuration) {
+	public init(configuration: EXHostViewController.Configuration) {
 		self.configuration = configuration
 	}
 
-	init(identity: AppExtensionIdentity, sceneID: String) {
+	public init(identity: AppExtensionIdentity, sceneID: String) {
 		self.init(configuration: .init(appExtension: identity, sceneID: sceneID))
 	}
 
