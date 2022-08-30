c36193ea6ceb0618f9ae32f0ac9dffc009bd9095
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-08-30 06:42:01 -0400

    AppExtensionBrowserView

---
+++ Sources/Extendable/AppExtensionBrowserView.swift
@@ -0,0 +1,12 @@
+import ExtensionKit
+import SwiftUI
+
+@available(macOS 13.0, *)
+public struct AppExtensionBrowserView: NSViewControllerRepresentable {
+	public func makeNSViewController(context: Context) -> EXAppExtensionBrowserViewController {
+		return EXAppExtensionBrowserViewController()
+	}
+
+	public func updateNSViewController(_ viewController: EXAppExtensionBrowserViewController, context: Context) {
+	}
+}
