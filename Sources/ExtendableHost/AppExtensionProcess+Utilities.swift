ed81589a7be2b990baf41cbefffd2ac19e669694
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-04-13 10:13:46 -0400

    Fix up availability

--- Sources/ExtendableHost/AppExtensionProcess+Utilities.swift
+++ Sources/ExtendableHost/AppExtensionProcess+Utilities.swift
@@ -1,6 +1,10 @@
+#if canImport(ExtensionKit)
 import ExtensionKit
 
 @available(macOS 13.0, *)
+@available(iOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
 extension AppExtensionProcess {
 	/// Async init that is compatible with an actor-isolated context.
 	public init(appExtensionIdentity identity: AppExtensionIdentity, onInterruption handler: @escaping @Sendable () -> Void = {}) async throws {
@@ -9,3 +13,4 @@ extension AppExtensionProcess {
 		try await self.init(configuration: config)
 	}
 }
+#endif
